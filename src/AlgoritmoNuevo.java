9f6b98c5e3dbd9191b8bc5492270d8a4d4e0e90d
denny <dennybaquerizo@hotmail.com>
2017-07-19 11:15:35 -0500

    enviando imprimir cambios

--- src/AlgoritmoNuevo.java
+++ src/AlgoritmoNuevo.java
@@ -4,7 +4,21 @@ public class AlgoritmoNuevo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ALGORITMO SALVAJE");
+		int i=0;
+		int nota=0;
+		boolean copiaEnExamen=true;
+		do{
+			System.out.println("Tu nota es"+ nota);
+			if(i>5){
+				break;
+			}
+			i++;
+		}while(copiaEnExamen);
 
 	}
-
+public static void imprimirTesis(){
+	System.out.println("blalalalalalala");
+	System.out.println("blalalalalalala");
+	System.out.println("mas lata");
+}
 }
